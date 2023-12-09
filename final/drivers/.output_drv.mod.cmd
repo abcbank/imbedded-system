1a6397d81f3680e8d18f0c73556f194118f4fbe5
abcbank <woejr_2012@naver.com>
2023-12-09 05:11:55 -0500

    wtf

---
+++ final/drivers/.output_drv.mod.cmd
@@ -0,0 +1,1 @@
+cmd_/home/kusw05/imbedded-system/final/drivers/output_drv.mod := { echo  /home/kusw05/imbedded-system/final/drivers/output_drv.o;  echo; } > /home/kusw05/imbedded-system/final/drivers/output_drv.mod
---
+++ final/drivers/output_drv.mod
@@ -0,0 +1,2 @@
+/home/kusw05/imbedded-system/final/drivers/output_drv.o
+
